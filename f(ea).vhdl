2372c05e30c1b935c372e6aa44db9478ce684637
NotRad <1vitor.camargo@gmail.com>
2023-06-20 23:06:12 -0300

    Axo que ta promto

--- f(ea).vhdl
+++ f(ea).vhdl
@@ -11,7 +11,12 @@ END ENTITY;
 ARCHITECTURE past OF estado_anterior IS
 
 BEGIN
-    c_out(2) <= (m_25 AND m_50) OR (c_in(0) AND m_50) OR (c_in(1) AND c_in(0) AND m_25);
-    c_out(1) <= (NOT(c_in(1)) AND m_25) OR (m_25 AND m_50) OR (c_in(1) AND NOT(m_25));
-    c_out(0) <= (m_25 AND m_50) OR (NOT(c_in(0)) AND m_50) OR (c_in(1) AND c_in(0) AND NOT(m_25)) OR (c_in(1) AND NOT(c_in(0)) AND m_25) OR (NOT(c_in(1)) AND c_in(0) AND NOT(m_50));
+    c_out(2) <= (m_50 and m_25) or (not(c_in(2)) and c_in(0) and m_50) or (not(c_in(2)) and c_in(1) and c_in(0) and m_25);
+    c_out(1) <= (not(c_in(2)) and c_in(1) and not(m_25)) or (not(c_in(1)) and m_25) or (m_50 and m_25) or (c_in(2) and m_25);
+    c_out(0) <= (not(c_in(2)) and c_in(0) and not(m_25) and not(m_50)) or (not(c_in(1)) and c_in(0) and m_25) or (c_in(1) and not(c_in(0)) and m_25) or (not(c_in(0)) and m_50) or (c_in(1) and m_50);
 END ARCHITECTURE;
+
+    --Abaixo a versao antiga que num funfa
+    --c_out(2) <= (m_25 AND m_50) OR (c_in(0) AND m_50) OR (c_in(1) AND c_in(0) AND m_25);
+    --c_out(1) <= (NOT(c_in(1)) AND m_25) OR (m_25 AND m_50) OR (c_in(1) AND NOT(m_25));
+    --c_out(0) <= (m_25 AND m_50) OR (NOT(c_in(0)) AND m_50) OR (c_in(1) AND c_in(0) AND NOT(m_25)) OR (c_in(1) AND NOT(c_in(0)) AND m_25) OR (NOT(c_in(1)) AND c_in(0) AND NOT(m_50));
--- tb_chocoMaq.vhdl
+++ tb_chocoMaq.vhdl
@@ -9,7 +9,7 @@ architecture tb_teste of tb_chocoMaq is
             c_in   : in  std_logic_vector(2 downto 0);
             d_dev  : out std_logic;
             d_cof  : out std_logic;
-            d_disp : out std_logic;
+            d_disp : out std_logic
         );
     end component;
     component estado_anterior is
@@ -17,7 +17,7 @@ architecture tb_teste of tb_chocoMaq is
             c_in   : in  std_logic_vector(2 downto 0);
             m_50  : in std_logic;
             m_25  : in std_logic;
-            c_out : out std_logic_vector(2 downto 0);
+            c_out : out std_logic_vector(2 downto 0)
         );
     end component;
     component bloco_reg is
@@ -25,20 +25,84 @@ architecture tb_teste of tb_chocoMaq is
             clk   : in  std_logic;
             reset : in  std_logic;
             c_in  : in  std_logic_vector(2 downto 0);
-            c_out : out std_logic_vector(2 downto 0);
+            c_out : out std_logic_vector(2 downto 0)
         );
     end component;
 
         signal sFEAtoREG, sREGtoEA : std_logic_vector(2 downto 0);
-        signal sclk: std_logic;
+        signal sClk : std_logic := '1';
+        signal sReset : std_logic;
+        signal sM50, sM25, sDev, sCof, sDisp : std_logic;
 
     begin
 
+        u_estadanterior : estado_anterior port map (sREGtoEA, sM50, sM25, sFEAtoREG);
+        u_registrator   : bloco_reg       port map (sClk, sReset, sFEAtoREG, sREGtoEA);
+        u_estadatual    : estado_atual    port map (sREGtoEA, sDev, sCof, sDisp);
 
+        u_testbantche : process
+        begin
+            sReset <= '0';
+            sM50   <= '0';
+            sM25   <= '0';
+            wait for 4*CLK_PERIOD;
+                --Instrunções de uso: um CLK_PERIOD equivale a um "estado" do sistema. Se, por exemplo, o
+                --sM25 ficar ativado ('1') por mais de um CLK_PERIOD, a máquina vai entender que foram adicionadas
+                --duas moedas de 25 centavos
 
-
-
-
+                --Teste da saida de erro
+                sReset <= '1';
+                sM50   <= '1';
+                sM25    <= '1';
+            wait for CLK_PERIOD;
+                sM50 <= '0';
+                sM25 <= '0';
+            wait for 4*CLK_PERIOD;
+                sM25 <= '1';
+            wait for CLK_PERIOD;
+                sM25 <= '0';
+            wait for CLK_PERIOD;
+                sM50 <= '1';
+            wait for CLK_PERIOD;
+                sM50 <= '0';
+            wait for CLK_PERIOD;
+                sM25 <= '1';
+            wait for CLK_PERIOD;
+                sM25 <= '0';
+            wait for CLK_PERIOD;
+                sM25 <= '1';
+            wait for CLK_PERIOD;
+                sM25 <= '0';
+            wait for CLK_PERIOD;
+                sM50 <= '1';
+            wait for CLK_PERIOD;
+                sM50 <= '0';
+            wait for CLK_PERIOD;
+                sM50 <= '1';
+            wait for CLK_PERIOD;
+                sM50 <= '0';
+            wait for CLK_PERIOD;
+                sM50 <= '1';
+            wait for 5*CLK_PERIOD;
+                sM25 <= '1';
+            wait for 2*CLK_PERIOD;
+                sM25 <= '0'; 
+            wait for CLK_PERIOD;
+                sM25 <= '1';
+            wait for CLK_PERIOD;
+                sM50 <= '0';
+                sM25 <= '0'; 
+            wait for CLK_PERIOD;
+                sM25 <= '1';
+            wait for 8*CLK_PERIOD;
+                sM25 <= '0';
+                sM50 <= '1'; 
+            wait for 6*CLK_PERIOD;
+                sM50 <= '0';
+                sM25 <= '1';
+            wait for 100*CLK_PERIOD; 
+        wait;
+        end process;
 
     p_clock : process
     begin
